4c78d62ac357f61829a38459e2509d4ad68f567c
William <Billy.campbell17@gmail.com>
2021-01-04 17:27:09 -0500

    updating porfolio

--- portfolio-x-2/src/App.js
+++ portfolio-x-2/src/App.js
@@ -1,15 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-         Hello World
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="app">
+     <div class="first-flex">
+  <div class="developer-name">
+    <h1>Billy Campbell</h1>
+    <p>Ruby on Rails | WeChat Miniprograms | Project Management </p>
+    <div class="icons">
+    <i class="fa fa-2x fa-github"></i>
+    <i class="fa fa-2x fa-medium"></i>
+    <i class="fa fa-2x fa-envelope-square"></i>
+    <i class="fa fa-2x fa-twitter"></i>
+    <i class="fa fa-2x fa-instagram"></i>
+  </div>
+
+  </div>
+  <a href="mailto:myemail">Let's Chat</a>
+</div>
+
+<div class="first-flex">
+  <h2>My Work</h2>
+  <div class="card">
+    <h6>Title</h6>
+    <p>Subtitle</p>
+  </div>
+  <div class="card">
+    <h6>Title</h6>
+    <p>Subtitle</p>  
+  </div>
+   <div class="card">
+    <h6>Title</h6>
+    <p>Subtitle</p>
+  </div>
+</div>
+
+        
       
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
